42e9e0c8276b1f0af7ff77c304b5cb99fb407676
Troels Bastrup Mikkelsen <troe2306@edu.campusvejle.dk>
2016-09-02 09:20:04 +0200

    Add project files.

---
+++ BB_LINQ/DB.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BB_LINQ {
+    public static class DB {
+        public const string connectionString = @"Data Source=CV-PC-T-04\SQLEXPRESS;Initial Catalog=BorgesBadevaerelser;Integrated Security=True";
+    }
+}
---
+++ BB_LINQ/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BB_LINQ {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private static string connectionString = @"Data Source=CV-PC-T-04\SQLEXPRESS;Initial Catalog=BorgesBadevaerelser;Integrated Security=True";
+
+        public MainWindow() {
+            InitializeComponent();
+
+            DataContext = new RammeAftaleViewModel(1);
+        }
+    }
+}
